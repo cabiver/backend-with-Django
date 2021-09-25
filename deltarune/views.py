4867ffccf1e11b5aa10cde4191c48c66655eb069
cabiver <carlosvergel01@gmail.com>
2021-09-25 16:07:35 -0500

    deltarune

---
+++ deltarune/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'deltarune.html')
+def big_shot(request):
+    return render(request, 'deltarune.html')
+def example(request):
+    return render(request, 'example.html')
+# Create your views here.
--- pleaseBeTrue/asgi.py
+++ pleaseBeTrue/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for pro project.
+ASGI config for pleaseBeTrue project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pro.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pleaseBeTrue.settings')
 
 application = get_asgi_application()
