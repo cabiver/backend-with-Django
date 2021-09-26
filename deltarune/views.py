32804239fcada24ee52b993b51cfec3980852bca
cabiver <carlosvergel01@gmail.com>
2021-09-26 18:53:53 -0500

    villan

--- deltarune/views.py
+++ deltarune/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'deltarune.html')
 def big_shot(request):
-    return render(request, 'deltarune.html')
+    return render(request, 'villanos.html')
 def example(request):
     return render(request, 'example.html')
+
 # Create your views here.
